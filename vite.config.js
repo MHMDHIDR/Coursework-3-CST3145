33791f9dbb351ff8fa12e7dd607ee3958d7b988a
Mohammed Haydar <mr.hamood277@gmail.com>
2024-03-01 11:03:56 +0000

    Update base URL and file paths

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue2 from '@vitejs/plugin-vue2'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Coursework-3-CST3145/',
+  base: '/',
   plugins: [
     vue2(),
     legacy({
